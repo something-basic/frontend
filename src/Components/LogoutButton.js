1da47fa8e871683875adea1b076cf18a325b17d6
Joseph Nguyen <42249177+3lueHippo@users.noreply.github.com>
2021-11-18 23:14:05 -0800

    Update LogoutButton.js

--- src/Components/LogoutButton.js
+++ src/Components/LogoutButton.js
@@ -5,7 +5,7 @@ export default class LogoutButton extends Component {
   render() {
     return (
       <GoogleLogout
-        clientId={`${process.env.REACT_APP_AUTH_DOMAIN}`}
+        clientId="707464855273-6v3nkb519bofnhh0b4ivuareaa1o7vvt.apps.googleusercontent.com"
         buttonText="Logout"
         onLogoutSuccess={() => {
           this.props.handleLogout();
