22a48e700af8be146b1584111a0720059d1f467e
Chloe Nott <chloe@nott.email>
2021-11-23 01:54:04 -0800

    Added placeholder subheading text for page load

--- src/Components/HeaderSubtitle.js
+++ src/Components/HeaderSubtitle.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../CSS/table.css";
+import Placeholder from 'react-bootstrap/Placeholder'
 
 export default class HeaderSubtitle extends Component {
   render() {
@@ -8,7 +9,7 @@ export default class HeaderSubtitle extends Component {
     const unreadChange = currentStats[1] ? currentStats[0].unread - currentStats[1].unread : 0
     return (
       <div>
-        {currentStats[0] && 
+        {currentStats[0] ? 
           <p style={{display: "block", margin: "1em 0 3em 0", textAlign: "center" }}>
             You have {currentStats[0].total} emails total, {currentStats[0].unread} unread emails, and<br/>
             {unreadChange < 0 ? 
@@ -17,6 +18,12 @@ export default class HeaderSubtitle extends Component {
                 `no change in unread emails since the last update ${lastUpdate}` : 
                 `${Math.abs(unreadChange)} more unread emails than the last update ${lastUpdate}`
             }
+          </p> :
+          <p style={{display: "block", margin: "1em 0 3em 0", textAlign: "center" }}>
+            <Placeholder animation="glow"> 
+              <Placeholder as="p" xs={2} />{' '}<Placeholder as="p" xs={2} />{' '}<Placeholder as="p" xs={1} /><br />
+              <Placeholder as="p" xs={3} />{' '}<Placeholder as="p" xs={1} />{' '}<Placeholder as="p" xs={2} />
+            </Placeholder>
           </p>
         }
       </div>
